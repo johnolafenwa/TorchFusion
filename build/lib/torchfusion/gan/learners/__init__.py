8add99a52f4328bb6e98b5b78fbb6b51bc02425c
= <=>
2018-10-16 23:20:07 +0100

    some fixes

--- build/lib/torchfusion/gan/learners/__init__.py
+++
@@ -1,1 +0,0 @@
-from .learners import StandardGanLearner,RStandardGanLearner,RAvgStandardGanLearner,WGanLearner,HingeGanLearner,RHingeGanLearner,BaseGanLearner,StandardBaseGanLearner,RAvgHingeGanLearner,BaseGanCore
